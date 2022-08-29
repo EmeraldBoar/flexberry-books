1d35bea453fff13736c0b0a5f062eeb43696f2a6
EmeraldBoar <kozlovkv174@gmail.com>
2022-08-29 15:00:13 +0300

    fix: rootURL in production

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'flexberry-books',
     environment,
-    rootURL: '/flexberry-books',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
@@ -45,6 +45,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV.rootURL = '/flexberry-books';
   }
 
   return ENV;
