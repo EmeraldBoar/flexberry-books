d48ad4a4943adb6b0bc06905cfebb3d1dbb2e65d
EmeraldBoar <kozlovkv174@gmail.com>
2022-09-16 10:49:12 +0300

    feat: add json server setting

--- app/services/data.js
+++ app/services/data.js
@@ -6,11 +6,14 @@ export default Service.extend({
   init() {
     this._super(...arguments);
     this.set('speakers', A());
+    this.set('books', A());
   },
 
-  async getSpeakers() {
+  async getSpeakers(searchValue) {
+    let searchQuery = searchValue ? `?q=${searchValue}` : '';
+
     try {
-      let response = await fetch(`${ENV.backendURL}/authors`);
+      let response = await fetch(`${ENV.backendURL}/speakers${searchQuery}`);
       let speakers = await response.json();
       this.get('speakers').clear();
       this.get('speakers').pushObjects(speakers);
@@ -21,26 +24,52 @@ export default Service.extend({
     }
   },
 
-  async getBooks() {
+  async getBooks(searchValue, tagsValue) {
+    let route = {
+      fullText: 'q=',
+      filter: '_like=',
+    }
+
+    let separator = '&';
+    let parameter = searchValue || tagsValue ? '?' : '';
+    let tagDataName = 'tags';
+
+    let searchQuery = searchValue ? `${route.fullText}${searchValue}` : '';
+    let tagsQuery = tagsValue ? `${tagDataName}${route.filter}${tagsValue}` : '';
+
+
+    let values = [searchQuery, tagsQuery];
+    let query = values.filter((query) => query).join(separator);
+
     try {
-      let response = await fetch(`${ENV.backendURL}/books`);
-      return response.json();
+      let response = await fetch(`${ENV.backendURL}/books${parameter}${query}`);
+      let speakers = await response.json();
+      this.get('books').clear();
+      this.get('books').pushObjects(speakers);
+      return this.get('books');
+
     } catch(error) {
       throw new Error(error);
     }
   },
 
-  getSpeaker(id) {
-    return this.get('speakers').find((speaker) => speaker.id === parseInt(id));
+  async getElement(id, routeName) {
+    let response = await fetch(`${ENV.backendURL}/${routeName}/${id}`);
+    return response.json();
   },
 
-  deleteSpeaker(speaker) {
-    return this.get('speakers').removeObject(speaker);
+  async deleteElement(route, element) {
+    this.get(route).removeObject(element);
+    try {
+      await fetch(`${ENV.backendURL}/${route}/${element.id}`, { method: 'DELETE' });
+    } catch(error) {
+      throw new Error(error);
+    }
   },
 
   async createSpeaker(speaker) {
     try {
-      await fetch(`${ENV.backendURL}/authors`, {
+      await fetch(`${ENV.backendURL}/speakers`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -51,5 +80,57 @@ export default Service.extend({
       throw new Error(error);
     }
 
-  }
+  },
+
+  async createBook(book, uploadData) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const savedBookPromise = await fetch(`${ENV.backendURL}/books`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(book)
+        });
+
+        const savedBook = await savedBookPromise.json();
+
+        if (!uploadData) {
+          resolve();
+        }
+
+        uploadData.url = `${ENV.fileUploadURL}`;
+        // uploadData.headers = getOwner(this).lookup('adapter:application').get('headers');
+        uploadData.submit().done(async (result/*, textStatus, jqXhr*/) => {
+          try {
+            const dataToUpload = {
+              entityName: 'books',
+              id: savedBook.id,
+              fileName: result.filename
+            };
+
+            await fetch(`${ENV.backendURL}/saveURL`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(dataToUpload)
+            });
+
+            // eslint-disable-next-line no-console
+            console.log('Ok');
+            resolve();
+          }
+          catch (e) {
+            reject(e);
+          }
+        }).fail((jqXhr, textStatus, errorThrown) => {
+          reject(errorThrown);
+        });
+      }
+      catch (e) {
+        reject(e);
+      }
+    });
+  },
 });
