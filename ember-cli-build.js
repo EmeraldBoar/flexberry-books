d2ba11ae13f35bc93ca22417225135595ab46914
EmeraldBoar <kozlovkv174@gmail.com>
2022-09-16 10:39:15 +0300

    feat: add vendor files to project

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const funnel = require('broccoli-funnel');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
@@ -10,18 +11,27 @@ module.exports = function(defaults) {
     }
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  app.import('vendor/tagsinput.css');
+  app.import('vendor/bootstrap-select.min.css');
+  app.import('vendor/bootstrap-datepicker.css');
 
-  return app.toTree();
+
+  const jsFiles = funnel('vendor', {
+    include: ['**/*.js'],
+    destDir: 'js'
+  });
+
+  const bootstrapFiles = funnel('node_modules/bootstrap/dist/js', {
+    include: ['bootstrap.bundle.min.js'],
+    destDir: 'js'
+  });
+
+  const jqueryFiles = funnel('node_modules/blueimp-file-upload/js', {
+    include: ['**/*.js'],
+    destDir: 'js'
+  });
+
+
+
+  return app.toTree([jsFiles, bootstrapFiles, jqueryFiles]);
 };
