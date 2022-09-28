2262f8c3285df9c0e578c7649ab905743785c9e1
EmeraldBoar <kozlovkv174@gmail.com>
2022-09-28 12:09:51 +0300

    feat: add report model

---
+++ app/models/report.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  presentationURL: DS.attr('string'),
+  videoURL: DS.attr('string'),
+  review: DS.attr('string'),
+  rating: DS.attr('number'),
+
+  meeting: DS.belongsTo('meeting'),
+  book: DS.belongsTo('book'),
+  speaker: DS.belongsTo('speaker')
+});
