e491471f20f9228b0307f092fb9929f19b1b8687
EmeraldBoar <kozlovkv174@gmail.com>
2022-09-28 12:07:18 +0300

    feat: add meetings-item component

---
+++ app/components/meetings-item.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  // didReceiveAttrs() {
+  //   this._super(...arguments);
+  //   console.log(this.get('report'));
+  // }
+});
