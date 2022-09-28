d46a17c93ca29711814e516ea2627a3e45c88f0a
EmeraldBoar <kozlovkv174@gmail.com>
2022-09-28 12:10:20 +0300

    feat: add relationships

--- app/models/speaker.js
+++ app/models/speaker.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
-  patronymic: DS.attr('string')
+  patronymic: DS.attr('string'),
+  reports: DS.hasMany('report')
 });
